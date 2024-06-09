bd22c1ad981d94c93bbd48618d1dfef69065e728
Dawid Budaszewski <dawid.budaszewski1@gmail.com>
2024-06-09 12:59:17 +0400

    add print log

--- ios/VisionCameraTextRecognition.swift
+++ ios/VisionCameraTextRecognition.swift
@@ -71,6 +71,7 @@ public class VisionCameraTextRecognition: FrameProcessorPlugin {
                 let buffer = frame.buffer
         let image = VisionImage(buffer: buffer)
         // image.orientation = getOrientation(orientation: frame.orientation)
+        print("orientation: \(frame.orientation)")
         image.orientation = frame.orientation
 
         var result: Text
